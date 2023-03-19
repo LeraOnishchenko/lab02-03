cf76a9cc60e9c0604a8a08cdb9a26d5d949cabcf
Vladyslav Zabrodskyi <vladyslav.zabrodskyi@fordewind.io>
2023-03-19 12:07:10 +0200

    hw02

---
+++ Tools/DataStorage/IDataStorage.cs
@@ -0,0 +1,13 @@
+﻿using KMA.ProgrammingInCSharp2023.Lab01.DateApp.Models;
+
+namespace KMA.ProgrammingInCSharp2023.Lab01.DateApp.Tools.DataStorage
+{
+    internal interface IDataStorage
+    {
+        bool UserExists(string login);
+
+        Person GetUserByLogin(string login);
+
+        void AddUser(Person user);
+    }
+}
---
+++ Views/Submited/InfoView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using KMA.ProgrammingInCSharp2023.Lab01.DateApp.Tools.Navigation;
+using KMA.ProgrammingInCSharp2023.Lab01.DateApp.ViewModels.PrecessedDate;
+
+namespace KMA.ProgrammingInCSharp2023.Lab01.DateApp.Views.Submited
+{
+    /// <summary>
+    /// Interaction logic for InfoView.xaml
+    /// </summary>
+    public partial class InfoView : UserControl,INavigatable
+    {
+        internal InfoView()
+        {
+            InitializeComponent();
+            DataContext = new InfoDateViewModel();
+        }
+    }
+}
